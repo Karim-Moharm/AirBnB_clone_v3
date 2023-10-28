c4faffcced827683cf904447b12320c85a2a01ce
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-28 19:18:26 +0200

    testing..

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,9 @@ import sys
 
 from flask import Flask, Blueprint
 from os import getenv
+from models import storage
 from api.v1.views import app_views
 # sys.path.append('../..')
-from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
