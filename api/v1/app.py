edcbdfe4ddd712b03eb875bcab82d01feeaa9878
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-28 18:32:50 +0200

    testing on sandbox

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
+
+import sys
+
 from flask import Flask, Blueprint
 from os import getenv
-from models import storage
 from api.v1.views import app_views
+# sys.path.append('../..')
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def close_session(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST")
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Blueprint
 from api.v1.views.index import *
+from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
