51da6c39ac9ea6e2d1dad9768e658d2a90911399
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-28 20:41:49 +0200

    stats function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,39 @@
 """import libraries"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.all_models import our_models
 
 
 @app_views.route("/status")
 def status():
     """returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """return json fromat of number of each objects by type
+    """
+
+    # json_format = jsonify({
+    #     "amenities": storage.count(Amenity),
+    #     "cities": storage.count(City),
+    #     "places": storage.count(Place),
+    #     "reviews": storage.count(Review),
+    #     "states": storage.count(State),
+    #     "users": storage.count(User)
+    # })
+
+    # return json_format
+
+    json_format = jsonify({
+        "amenities": storage.count(our_models['Amenity']),
+        "cities": storage.count(our_models['City']),
+        "places": storage.count(our_models['Place']),
+        "reviews": storage.count(our_models['Review']),
+        "states": storage.count(our_models['State']),
+        "users": storage.count(our_models['User'])
+    })
+
+    return json_format
