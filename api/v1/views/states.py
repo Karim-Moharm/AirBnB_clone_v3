c3898785f4cfe7c7ca55116a5be4aeb053fa7b2d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-28 21:44:46 +0200

    upload

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 """
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, abort
 from models.all_models import our_models
 from models.state import State
 
@@ -13,8 +13,11 @@ def get_states():
     """return json format for states object
     """
     states_list = []
-    for item in storage.all('State').values():
+    for item in storage.all(State).values():
         states_list.append(item.to_dict())
     return jsonify(states_list)
-    #     states_list.append(item)
-    # return states_list
+
+@app_views.route("/api/v1/states/<state_id>", methods=['GET'])
+def get_states_id(state_id):
+    state = storage.get(State, state_id)
+    return jsonify(state.to_dict())
